c7221e9fae645ab6d0d6e30929419540a4b365ea
Fullstack web developer <abrorizainal263@gmail.com>
2025-09-25 09:23:11 +0700

    First step

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'notes-v1';
+const appShellFiles = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return r || fetch(e.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
